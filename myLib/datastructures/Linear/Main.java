3b3d8cef5a329c0d24f445692662ca7e264d63d3
MacKante <mackante.git@gmail.com>
2023-04-03 14:32:27 -0600

    fixed queue finally

--- myLib/datastructures/Linear/Main.java
+++ myLib/datastructures/Linear/Main.java
@@ -12,15 +12,31 @@ public class Main {
         SNode test3 = new SNode(3);
         SNode test4 = new SNode(4);
         SNode test5 = new SNode(5);
+        SNode test6 = new SNode(6);
+        SNode test7 = new SNode(7);
+        SNode test8 = new SNode(8);
+        SNode test9 = new SNode(9);
 
+        SLL testSLL = new SLL();
+        testSLL.InsertHead(test6);
+        testSLL.InsertHead(test7);
+        testSLL.InsertTail(test9);
+        testSLL.Insert(test8, 2);
+        testSLL.Print();
+        testSLL.Clear();
+        System.out.println();
+        testSLL.Print();
+        System.out.println();
         test.Enqueue(test5);
         test.Enqueue(test4);
         test.Enqueue(test3);
         test.Enqueue(test2);
         test.Enqueue(test1);
         test.Dequeue();
-
-        System.out.println(test.Seek(test3));
+        test.Dequeue();
         test.Print();
+        test.Clear();
+        test.Print();
+
     }
 }
