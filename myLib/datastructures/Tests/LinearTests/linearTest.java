846da7c6d2bf3cf14e0672e9ed694fc0b78fccf1
MacKante <114007326+MacKante@users.noreply.github.com>
2023-04-08 13:07:31 -0600

    saving changes to be safe

--- myLib/datastructures/Tests/LinearTests/linearTest.java
+++ myLib/datastructures/Tests/LinearTests/linearTest.java
@@ -3,7 +3,7 @@ import myLib.datastructures.Linear.*;
 import myLib.datastructures.nodes.DNode;
 public class linearTest {
     public static void main(String[] args) {
-        DLL test = new DLL();
+        QueueLL test = new QueueLL();
         DNode node0 = new DNode(0);
         DNode node1 = new DNode(1);
         DNode node2 = new DNode(2);
@@ -12,16 +12,13 @@ public class linearTest {
         DNode node5 = new DNode(5);
         DNode node6 = new DNode(6);
 
-        test.Insert(node6, 1);
-        test.InsertTail(node5);
-        test.InsertTail(node2);
-        test.InsertHead(node3);
-        test.InsertTail(node4);
-        test.Insert(node0, 4);
-        test.Print();
-        test.DeleteHead();
-        test.DeleteTail();
-        test.Sort();
+        test.Enqueue(node6);
+        test.Enqueue(node5);
+        test.Enqueue(node2);
+        test.Enqueue(node3);
+        test.Enqueue(node4);
+        test.Enqueue(node0);
+        test.Dequeue();
         test.Print();
 
     }
